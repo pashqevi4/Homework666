ad8ef6869055bb2492f29509d01b07aac242b98b
pashqa <pashqevi4@gmail.com>
2023-03-19 22:18:00 +0700

    Task 2 complete

---
+++ task2/Program.cs
@@ -0,0 +1,21 @@
+﻿//  Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+// 452 -> 11
+
+// 82 -> 10
+
+// 9012 -> 12
+
+Console.WriteLine("Enter Your Number");
+int A = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+int Division(int A)
+{
+    while (A > 2)
+    {
+        sum = sum + (A % 10);
+        A = A / 10;
+    }
+    return sum;
+}
+Console.WriteLine($"{Division(A)}");
